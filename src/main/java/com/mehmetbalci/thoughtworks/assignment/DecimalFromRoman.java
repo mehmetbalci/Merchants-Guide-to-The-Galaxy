5767e135e26ea911a7e4645e740819f38c1cb339
mehmetbalci <39764008+mehmetbalci@users.noreply.github.com>
2021-10-06 12:09:34 +0200

    validateRepeatations function is set to check validations X, C, I, M

--- src/main/java/com/mehmetbalci/thoughtworks/assignment/DecimalFromRoman.java
+++ src/main/java/com/mehmetbalci/thoughtworks/assignment/DecimalFromRoman.java
@@ -35,17 +35,48 @@ public class DecimalFromRoman {
         }
     }
 
+    // If X, C, I and M can be repeated three times
+    public static boolean validateRepeatations(String roman) {
+        //roman numbers as a char list
+        char[] result = roman.toCharArray();
+        //iterators for array
+        int i, j;
+        // Check if the number of length is greater than 3 or not
+        if (result.length > 3) {
+            for (i = 0; i < result.length - 3; i++) {
+                // If sucessive 3 are equal. i, i+1 and i+2 corresponds
+                if ((result[i] == result[i + 1]) && (result[i + 1] == result[i + 2])) {
+                    // If 4 in sucession, return false cause failed (i+3)
+                    if (result[i] == result[i + 3]) {
+                        return false;
+                    }
+                    if (result.length > i + 5) {
+                        // If 1, 2, 3 and 5th are equal, check if 4th one less than 5th or not
+                        if (result[i + 4] == result[i]) {
+                            // If 3th greater than 4th, fail
+                            if (romanMapping.get(Character.toString(result[i + 3])) > romanMapping
+                                    .get(Character.toString(result[i + 4]))) {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }}
+
+    // check the duplicate numbers for repetations and validate
+    public static boolean validateNoRepeatations
+    {
+    }
 
     // Validate the roman number rules and then convert them to decimal equivalents
     public static int convertToDecimal{
         
     }
 
-    // If X, C, I and M can be repeated three times
-    public static boolean validateRepeatations
-    {
-    }
-
+    
     //calculated complicated decimal numbers
     public static int calculateDecimal
     {
@@ -53,10 +84,7 @@ public class DecimalFromRoman {
 
     
 
-    // check the duplicate numbers for repetations and validate
-    public static boolean validateNoRepeatations
-    {
-    }
+    
 
     
 }
