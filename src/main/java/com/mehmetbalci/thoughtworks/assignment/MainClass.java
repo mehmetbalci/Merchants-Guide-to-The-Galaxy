5ffb40bd407273caa9c3b7101dc5abb6b262eb90
mehmetbalci <39764008+mehmetbalci@users.noreply.github.com>
2021-10-06 11:32:45 +0200

    main class initiialized

--- src/main/java/com/mehmetbalci/thoughtworks/assignment/MainClass.java
+++ src/main/java/com/mehmetbalci/thoughtworks/assignment/MainClass.java
@@ -1,8 +1,20 @@
 package com.mehmetbalci.thoughtworks.assignment;
 
 public class MainClass {
-
+    //read sample text from file and process it
     public static void main(String[] args) {
-   
+        String fileLocation = null;
+        if (args.length > 0) 
+        {
+            fileLocation = args[0];
+        }
+        try 
+        {
+            Input.ProcessInputFile(fileLocation);
+        } 
+        catch (Exception e) 
+        {
+            System.out.println("Text file not found in directory!!");
+        }
     }
 }
