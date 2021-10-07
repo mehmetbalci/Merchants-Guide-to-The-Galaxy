4ed46732d5ef5041595590d0f8b198043d6b3d11
mehmetbalci <39764008+mehmetbalci@users.noreply.github.com>
2021-10-07 16:13:07 +0200

    test class updated

--- src/test/java/com/mehmetbalci/thoughtworks/assignment/TestClass.java
+++ src/test/java/com/mehmetbalci/thoughtworks/assignment/TestClass.java
@@ -1,6 +1,17 @@
 package com.mehmetbalci.thoughtworks.assignment;
 
-
+import org.junit.Test;
 public class TestClass {
-
+    @Test
+    //This test should return the roman numeral numbers
+    public void romanShouldBeConvertedToDecimal() {
+        System.out.println(DecimalFromRoman.calculateDecimal("IX")); 
+    }
+    @Test
+    //Test should return sentence defined types
+    public void findSentenceType() {
+        String sentenceToEvaluate = "pish is L";
+        // Returns Roman Numeral equivalnets
+        System.out.println("\"" + sentenceToEvaluate + "\" is of type " + Input.classifyLines(sentenceToEvaluate)); 
+    }
 }
